5fc1d98bb59f4aecf38719264c2a72b1cd7ef8b4
nishant.kumar <knishant362@gmail.com>
2024-01-14 03:44:37 +0530

    add EntityContainer and Toggle Composable

---
+++ visionpad/src/main/java/dev/trendster/visionpad/components/EntityContainer.kt
@@ -0,0 +1,184 @@
+package dev.trendster.visionpad.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.trendster.visionpad.R
+import androidx.compose.ui.unit.Constraints
+
+
+/**                                         EntityContainer
+
+        +---------------------------------------------------------------------------------------+
+        |                                                                                       |
+        |  +-----------------+                                                                  |
+        |  |    Toggle (X)   |                                                                  |
+        |  |     (Delete)    |                                                                  |
+        |  +-----------------+                                                                  |
+        |                       +-----------------------------------------+                     |
+        |                       |            Container Border             |                     |
+        |                       |                                         |                     |
+        |                       |     +-----------------------------+     |                     |
+        |                       |     |                             |     |                     |
+        |                       |     |           Entity            |     |                     |
+        |                       |     |                             |     |                     |
+        |                       |     +-----------------------------+     |                     |
+        |                       |                                         |                     |
+        |                       +-----------------------------------------+                     |
+        |                                                                   +----------------+  |
+        |                                                                   |   Toggle (⤢)   |  |
+        |                                                                   |    (Resize)    |  |
+        |                                                                   +----------------+  |
+        |                                                                                       |
+        +---------------------------------------------------------------------------------------+
+
+ * */
+
+
+/**
+ * EntityContainer represents a container for an entity with toggle buttons.
+ * The container can be selected, and toggle buttons are displayed accordingly.
+ */
+
+@Composable
+internal fun EntityContainer(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    entity: @Composable () -> Unit,
+) {
+
+    /** Composable for the delete toggle button. */
+    val deleteToggle = @Composable {
+        Toggle(
+            icon = painterResource(id = R.drawable.close),
+            modifier = Modifier)
+    }
+
+    /** Composable for the scale toggle button. */
+    val scaleToggle = @Composable {
+        Toggle(icon = painterResource(id = R.drawable.resize),
+            modifier = Modifier)
+    }
+
+    /** Composable for the container's border.*/
+    val containerBorder = @Composable {
+        Box(
+            modifier = Modifier
+                .border(
+                    width = 2.dp,
+                    color = if (isSelected) Color.White else Color.Transparent,
+                    shape = RoundedCornerShape(1.dp),
+                )
+                .fillMaxSize()
+        )
+    }
+
+
+    /** Define a Composable for rendering the toggle buttons based on selection.
+     * [isSelected] = true : show Actual Toggle
+     * [isSelected] = false : show Dummy Spacer
+     * */
+
+    val toggles = @Composable {
+        if (isSelected) {
+            repeat(2) { index ->
+                when (index) {
+                    0 -> deleteToggle()
+                    1 -> scaleToggle()
+                }
+            }
+        } else repeat(2) {
+            Spacer(modifier = Modifier.size(ToggleSize))
+        }
+    }
+
+    /** Define the layout using the Layout Composable function. */
+    Layout(
+        contents = listOf(entity, toggles, containerBorder),
+        modifier = modifier
+    ) { (entityMeasurables, toggleMeasurables, borderMeasurables), constraints ->
+
+        // Ensure that the entity emits only one Composable.
+        require(entityMeasurables.size == 1) {"entity should only emit one composable"}
+
+
+        /** Measure the entity Composable and toggle buttons. */
+        val entityPlaceable = entityMeasurables.first().measure(constraints)
+        val togglePlaceables = toggleMeasurables.map { it.measure(constraints) }
+        val borderPlaceable = borderMeasurables.first().measure(
+            Constraints.fixed(
+                entityPlaceable.width + togglePlaceables[0].width,
+                entityPlaceable.height + togglePlaceables[0].height
+            )
+        )
+
+        /** Calculate the total width and height of the layout. */
+        val width = entityPlaceable.width + 2 * togglePlaceables[0].width
+        val height = entityPlaceable.height + 2 + togglePlaceables[0].height
+
+        /** Perform the layout positioning for the Composables. */
+        layout(
+            width = width,
+            height = height
+        ) {
+
+            val toggleOne = togglePlaceables[0]
+            val toggleTwo = togglePlaceables[1]
+
+            /** Position the border and toggle buttons relative to the entity. as shown in Diagram */
+            borderPlaceable.place(toggleOne.width / 2, toggleOne.height / 2)
+            toggleOne.place(0, 0)
+            entityPlaceable.place(toggleOne.width, toggleOne.height)
+            toggleTwo.place(
+                toggleOne.width + entityPlaceable.width,
+                toggleOne.height + entityPlaceable.height
+            )
+        }
+    }
+
+}
+
+
+@Composable
+@Preview
+internal fun EntityContainerPreview() {
+    // Example usage of EntityContainer in a Compose hierarchy
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .padding(16.dp)
+    ) {
+        EntityContainer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            isSelected = true,
+            entity = {
+                // Your entity content goes here
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Blue)
+                ) {
+                    // Your entity content
+                }
+            }
+        )
+    }
+}
---
+++ visionpad/src/main/java/dev/trendster/visionpad/components/Toggle.kt
@@ -0,0 +1,32 @@
+package dev.trendster.visionpad.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.trendster.visionpad.R
+
+
+val ToggleSize = 56.dp
+@Composable
+internal fun Toggle(
+    modifier: Modifier = Modifier,
+    icon: Painter
+){
+    Image(
+        painter = icon,
+        contentDescription = "Toggle Button",
+        modifier = modifier.size(ToggleSize)
+    )
+
+}
+
+@Preview
+@Composable
+fun TogglePreview() {
+    Toggle(icon = painterResource(id = R.drawable.baseline_3d_rotation_24), modifier = Modifier)
+}
